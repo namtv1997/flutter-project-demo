d36c2b25c54d4762217f8de733e0781d55853648
namtv1997 <namtv1997>
2021-08-23 14:21:27 +0700

    update

--- furniture_app_ui/lib/services/fetCategories.dart
+++ furniture_app_ui/lib/services/fetCategories.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 Future<List<Category>> fetchCategories() async {
   const String apiUrl =
       "https://5f210aa9daa42f001666535e.mockapi.io/api/categories";
-  final response = await http.get(apiUrl);
+  final response = await http.get(Uri.parse(apiUrl));
 
   if (response.statusCode == 200) {
     List<Category> categories = (json.decode(response.body) as List)
--- furniture_app_ui/lib/services/fetchProducts.dart
+++ furniture_app_ui/lib/services/fetchProducts.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 Future<List<Product>> fetchProducts() async {
   const String apiUrl =
       "https://5f210aa9daa42f001666535e.mockapi.io/api/products";
-  final response = await http.get(apiUrl);
+  final response = await http.get(Uri.parse(apiUrl));
 
   if (response.statusCode == 200) {
     List<Product> products = (json.decode(response.body) as List)
